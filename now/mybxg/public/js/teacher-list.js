4b2da1c960606fc1c75505d99e816f57b3ba515c
sunshinegff <435073910@qq.com>
2017-09-21 18:27:13 +0800

    讲师登录和注销功能完成

--- now/mybxg/public/js/teacher-list.js
+++ now/mybxg/public/js/teacher-list.js
@@ -8,6 +8,35 @@ define(['jquery','template'],function($,template){
 			// 解析数据，渲染页面
 			var teacherHtml=template('teacherTpl',{list:data.result});
 			$('#teacherInfo').html(teacherHtml);
+
+			// 启用和注销功能
+			$('.eod').click(function(){
+				var that=this;
+				// var td=$(this).parent();
+				var td=$(this).closest('td');  //closest查找离他最近的父元素
+				var tcId=td.attr('data-tcid');
+				var tcStatus=td.attr('data-status');
+				// 调用后台接口
+				$.ajax({
+					type: 'post',
+					url: '/api/teacher/handle',
+					data: {tc_id:tcId, tc_status:tcStatus},
+					dataType: 'json',
+					success: function(data){
+						if(data.code==200){
+							td.attr('data-status',data.result.tc_status);
+							if(data.result.tc_status==0){
+								$(that).text('启用');
+							}else{
+								$(that).text('注销');
+							}
+
+						}
+					}
+				})
+			})
 		}
-	})
+
+
+	});
 });
