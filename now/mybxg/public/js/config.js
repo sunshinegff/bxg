014763dd26a5cfaa669a7cf5fcbd926dbe604bbc
sunshinegff <435073910@qq.com>
2017-09-22 15:48:36 +0800

    讲师添加编辑页面-入职时间功能完成

--- now/mybxg/public/js/config.js
+++ now/mybxg/public/js/config.js
@@ -5,6 +5,8 @@ require.config({
 		cookie: 'jquery-cookie/jquery.cookie',
 		template: 'artTemplate/template-web',
 		bootstrap: 'bootstrap/js/bootstrap',
+		datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
+		language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
 		util: '../js/util',
 		common: '../js/common',
 		login: '../js/login',
@@ -12,8 +14,11 @@ require.config({
 		teacheradd: '../js/teacher-add',
 	},
 	shim: {
-		bootstrap :{
+		bootstrap: {
 			deps: ['jquery']
+		},
+		language: {
+			deps: ['jquery','datepicker']
 		},
 	}
 });
