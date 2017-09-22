74a740f8dd261364d7fc5c39ee7012be09252a26
sunshinegff <435073910@qq.com>
2017-09-22 21:09:49 +0800

    头像上传文件功能完成

--- now/mybxg/public/js/settings.js
+++ now/mybxg/public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','region'],function($,template){
+define(['jquery','template','uploadify','region'],function($,template){
 	// 调用接口获取所有的个人信息
 	$.ajax({
 		url: '/api/teacher/profile',
@@ -8,6 +8,21 @@ define(['jquery','template','region'],function($,template){
 			// console.log(data);
 			var html=template('settingsTpl',data.result);
 			$('#settingsInfo').html(html);
+
+			// 处理头像上传
+			$('#upfile').uploadify({
+				width: 120,
+				height: 120,
+				buttonText: '',
+				swf: '/public/assets/uploadify/uploadify.swf',
+				uploader: '/api/uploader/avatar',
+				fileObjName: 'tc_avatar',
+				onUploadSuccess: function(a,b){
+					// console.log(b);
+					var obj=JSON.parse(b);
+					$('.preview img').attr('src',obj.result.path);
+				}
+			});
 
 			// 处理省市县三级联动
 			$('#pcd').region({
