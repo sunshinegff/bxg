f63333fa007960768f54b37f225dd3b233b4fe87
sunshinegff <435073910@qq.com>
2017-09-25 17:40:31 +0800

    课程信息填充完成

--- now/mybxg/public/js/course-basic.js
+++ now/mybxg/public/js/course-basic.js
@@ -21,6 +21,25 @@ define(['jquery','template','util'],function($,template,util){
 			// 解析数据填充页面
 			var html=template('coursebasicTpl',data.result);
 			$('#coursebasicInfo').html(html);
+
+			// 处理二级分类的下拉联动操作
+			$('#firstType').change(function(){
+				// 获取一级分类ID
+				var pid=$(this).val();
+				// 根据一级分类的ID查询左右的二级分类数据
+				$.ajax({
+					type: 'get',
+					url: '/api/category/child',
+					data: {cg_id : pid},
+					dataType: 'json',
+					success: function(data){
+						var tpl = '<option value="">请选择二级分类...</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                        var html = template.render(tpl,{list:data.result});
+                        console.log(html);
+                        $('#secondType').html(html);
+					}
+				})
+			})
 		}
 	});
 
