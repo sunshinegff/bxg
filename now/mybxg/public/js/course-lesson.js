e4e777bc1486ac2f62833e99f174bf28acac2e45
sunshinegff <435073910@qq.com>
2017-09-27 21:06:19 +0800

    课时信息添加功能完成

--- now/mybxg/public/js/course-lesson.js
+++ now/mybxg/public/js/course-lesson.js
@@ -1,4 +1,19 @@
 define(['jquery','template','util'],function($,template,util){
 	// 设置导航菜单选中
 	util.setMenu('/course/add');
+	// 获取课程ID
+	var csId=util.qs('cs_id');
+	// 获取所有的课时列表数据
+	$.ajax({
+		type: 'get',
+		url: '/api/course/lesson',
+		data: {cs_id:csId},
+		dataType: 'json',
+		success: function(data){
+			// console.log(data);
+			// 解析数据，渲染页面
+			var html=template('lessonTpl',data.result);
+			$('#lessonInfo').html(html);
+		}
+	});
 });
